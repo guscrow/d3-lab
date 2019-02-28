c70591ffa6fdb0e6c786c8c23c7fc080b026b99b
guscrow <47224677+guscrow@users.noreply.github.com>
2019-02-28 15:09:11 -0500

    Create animals.js

---
+++ animals.js
@@ -0,0 +1,31 @@
+var animalMaker = function(name,color,cuteness)
+{
+	return {
+		name: name,
+		color: color,
+		cute:cuteness
+	}
+}
+
+var data = [
+	animalMaker("Puppy","white","10"),
+	animalMaker("Red Panda","Red","10"),
+	animalMaker("angler fish","grey","0")
+];
+
+var table = d3.select("body").append("table");
+
+var rows = table.selectAll("tr")
+		.data(data)
+		.enter()
+		.append("tr")
+
+
+rows.append("td")
+	.text(function(d) {return d.name});
+
+rows.append("td")
+	.text(function(d) {return d.color});
+
+rows.append("td")
+	.text(function(d) {return d.cute});
